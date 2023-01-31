236a7c55e3bd13e1635129e984d01a78a440892c
MiJaJo <tasksystem.john@gmail.com>
2023-01-31 21:42:21 +0900

    マニュアルへのリンク追加 / 一部のドメイン表示箇所をタイトルに変更 / 制限が公開設定かつ制限が0件のとき、aboutの「制限中のサーバー」が不自然に空白になる不具合の修正

--- app/javascript/mastodon/initial_state.js
+++ app/javascript/mastodon/initial_state.js
@@ -81,6 +81,7 @@
  * @property {boolean=} use_pending_items
  * @property {string} version
  * @property {boolean} translation_enabled
+ * @property {string} manual_url
  */
 
 /**
@@ -134,5 +135,6 @@ export const usePendingItems = getMeta('use_pending_items');
 export const version = getMeta('version');
 export const translationEnabled = getMeta('translation_enabled');
 export const languages = initialState?.languages;
+export const manual_url = getMeta('manual_url');
 
 export default initialState;
